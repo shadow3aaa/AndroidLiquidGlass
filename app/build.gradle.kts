0994991c532c3448275818748823782fc3495e62
Kyant0 <kyant2021@outlook.com>
2025-06-19 12:01:29 +0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
