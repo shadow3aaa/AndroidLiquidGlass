4e2c1a10cbed82148e88c93da1f5ac2935d8d33c
Kyant0 <kyant2021@outlook.com>
2025-07-04 13:59:56 +0800

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,9 +95,4 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.material.ripple)
-    implementation("androidx.compose.runtime:runtime") {
-        version {
-            strictly("1.9.0-alpha04")
-        }
-    }
 }
