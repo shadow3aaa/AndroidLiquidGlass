39fe48262f4c95a0ff5380c600b9f35bb9812663
Kyant0 <kyant2021@outlook.com>
2025-07-18 10:32:49 +0800

    Upload new demo APK and library AAR

--- app/src/main/java/com/kyant/glass/MainActivity.kt
+++ app/src/main/java/com/kyant/glass/MainActivity.kt
@@ -7,17 +7,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.LocalOverscrollFactory
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.material.ripple.RippleAlpha
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.WindowCompat
 import com.kyant.expressa.m3.LocalColorScheme
 import com.kyant.expressa.m3.color.ColorScheme
-import com.kyant.expressa.overscroll.rememberOffsetOverscrollFactory
+import com.kyant.expressa.overscroll.OffsetOverscrollFactory
 import com.kyant.expressa.prelude.*
 import com.kyant.expressa.ripple.LocalRippleConfiguration
 import com.kyant.expressa.ripple.RippleConfiguration
@@ -42,7 +44,10 @@ class MainActivity : ComponentActivity() {
                     )
                 )
             }
-            val overscrollFactory = rememberOffsetOverscrollFactory()
+            val overscrollFactory = OffsetOverscrollFactory(
+                orientation = Orientation.Vertical,
+                animationScope = rememberCoroutineScope()
+            )
 
             CompositionLocalProvider(
                 LocalColorScheme provides ColorScheme.systemDynamic()
